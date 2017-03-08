8ee29ad4fccb7fdaca0c0f955dd1a9b5d04743b8
Washington Soares <washingtonudi@gmail.com>
2017-03-08 09:09:31 -0300

    Componentes pai e filho

---
+++ scripts/silvaFamily.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Member from './member'
+
+export default () => (
+  <div>
+    <Member name='João' lastName='da Silva' />
+    <Member name='José' lastName='da silva' />
+    <Member name='Maria' lastName='da silva' />
+  </div>
+)
