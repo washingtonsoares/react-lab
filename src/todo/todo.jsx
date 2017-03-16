dee336cd5f34825d47b5c54c739aaf6069130f5f
Washington Soares <washingtonudi@gmail.com>
2017-03-15 23:55:51 -0300

    Criando mais alguns componentes

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 import PageHeader from '../template/pageHeader'
+import TodoForm from './todoForm'
+import TodoList from './todoList'
 
 class Todo extends Component {
   render () {
     return (
-      <PageHeader name='Tarefas' small='Cadastro' />
+      <div>
+        <PageHeader name='Tarefas' small='Cadastro' />
+        <TodoForm />
+        <TodoList />
+      </div>
     )
   }
 }
---
+++ src/todo/todoList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+  <div>
+    <ul>
+      <li>tarefa 1</li>
+      <li>tarefa 2</li>
+    </ul>
+  </div>
+)
